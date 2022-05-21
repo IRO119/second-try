20f6d82434b57818746f7a7d1fe731298e7b02c4
IRO119 <104471336+IRO119@users.noreply.github.com>
2022-05-20 20:34:34 -0600

    Add files via upload

---
+++ Tutorial.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+require("dotenv").config();
+const TOKEN  = "OTY4NjYzODUzNTc3Njk1MjMy.YmiIgw.hl1YHha-Ldi8EYdk1IRaquUkuSY";
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+});
+
+client.on("ready", () =>{
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) =>{
+    if(message.content == "hi"){
+        message.reply("Hi, looser");
+        
+    }
+
+});
+
+client.login(process.env.TOKEN);
